be6dc8b7b542250ac737bc87550fec8ef0c2ab5a
♦Venen Jean♦ <102526504+Savageboitim13@users.noreply.github.com>
2023-10-15 10:40:00 +0200

    Add files via upload

---
+++ Liskov Substitution/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Liskov_Substitution {
+    class Program {
+        static void Main(string[] args) {
+            IManager accountVIP = new CEO {
+                FirstName = "Emma",
+                LastName = "Stone"
+            };
+            accountVIP.CalculatePerHourRate(4);
+
+            Employee emp = new Manager {
+                FirstName = "Tim",
+                LastName = "Corey"
+            };
+            emp.AssignManager(accountVIP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{ emp.FirstName }'s sallary is ${ emp.Salary }/hour.");
+
+            Console.ReadLine();
+        }
+    }
+}
