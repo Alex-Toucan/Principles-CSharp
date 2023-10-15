585a4fbf4c8563a2b87cc558873c1039121528a8
♦Venen Jean♦ <102526504+Savageboitim13@users.noreply.github.com>
2023-10-15 10:45:58 +0200

    Add files via upload

---
+++ Dependency Inversion/Factory.cs
@@ -0,0 +1,20 @@
+namespace Dependency_Inversion {
+    class Factory {
+        public static IPerson CreatePerson() {
+            return new Person();
+        }
+
+        public static IChore CreateChore() {
+            return new Chore(CreateLogger(), CreateMessageSender());
+        }
+
+        public static ILogger CreateLogger() {
+            return new Logger();
+        }
+
+        public static IMessageSender CreateMessageSender() {
+            return new Texter();
+            // return new Emailer();
+        }
+    }
+}
---
+++ Dependency Inversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dependency_Inversion {
+    class Program {
+        public static void Main(string[] args) {
+            IPerson owner = Factory.CreatePerson();
+            owner.FirstName = "Tim";
+            owner.LasName = "Corey";
+            owner.EmailAddress = "tim@iamtimcoray.com";
+            owner.PhoneNumber = "555-1212";
+
+            IChore chore = Factory.CreateChore();
+            chore.ChoreName = "Take out the trash";
+            chore.Owner = owner;
+
+            chore.PerformWork(3);
+            chore.PerformWork(1.5);
+            chore.CompleteChore();
+
+            Console.ReadLine();
+        }
+    }
+}
