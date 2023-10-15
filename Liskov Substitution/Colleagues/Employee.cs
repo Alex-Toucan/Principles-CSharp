ad58c066da94eb92292e79cb463495e75ebee3d4
♦Venen Jean♦ <102526504+Savageboitim13@users.noreply.github.com>
2023-10-15 10:41:28 +0200

    Add files via upload

---
+++ Liskov Substitution/Colleagues/Employee.cs
@@ -0,0 +1,11 @@
+namespace Liskov_Substitution {
+    class Employee : BaseEmployee, IManaged {
+        public IEmployee Manager { get; set; } = null;
+
+        public virtual void AssignManager(IEmployee manager) {
+            // Simulate doing other tasks here - otherwise, this should be
+            // a property set statement, not a method!
+            Manager = manager;
+        }
+    }
+}
---
+++ Liskov Substitution/Colleagues/Manager.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Liskov_Substitution {
+    internal class Manager : Employee, IManager {
+        public override void CalculatePerHourRate(int rank) {
+            decimal baseAmount = 19.75M;
+
+            Salary = baseAmount + (rank * 4);
+        }
+
+        public void GeneratePerformanceReview() {
+            Console.WriteLine("I'm reviewing a direct report's performance.");
+        }
+    }
+}
